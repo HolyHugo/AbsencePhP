31b16de52e31189bedf59764b7cd40e22eaeac8e
holyhugo <hugobocktaels@gmail.com>
2017-09-21 11:34:51 +0200

    Ajout du choix des motifs et insertion d'un jour d'absence par défaut

--- Controleur/FormulaireAbsence.php
+++ Controleur/FormulaireAbsence.php
@@ -12,6 +12,7 @@ catch(Exception $e)
  
 echo'<html style="background-color:#CFFF98;">';
 echo'<h1 style="color:#7b95cd;">Ajouter une absence</h1>';
+
 $reponse = $bdd->query('SELECT * FROM Visiteur');
  
 
@@ -24,19 +25,30 @@ $reponse = $bdd->query('SELECT * FROM Visiteur');
 ?>
 	<option value="<?php echo $donnees['idVisiteur']; ?>"> <?php echo $donnees['nomV']; ?></option>
 	<?php
-	
+	 }
 	?>
-<?php  
-}
+
+	</select>
+<?php $reponse2 = $bdd->query('SELECT * FROM Motif') ?>
+<label for="libelle" style="color:#B01919;">Pourquoi ? : </label>
+<select name="libelle" id="idMotif">
+<?php
+		while ($donnees = $reponse2->fetch()){
 ?>
+
+		<option value="<?php echo $donnees['idMotif']; ?>"> <?php echo $donnees['libelle']; ?></option>
+		<?php
+		}
+		?>
 	</select>
 	<input type="submit" value="Envoyer"/>
 	</form>    
 <?php
 	$reponse->closeCursor();
+	$reponse2->closeCursor();
 //Affichage des absences d'hier
 
-	echo"Absence d'hier";
+	echo"<h3>Absence d'hier :</h3>";
 	$reponse = $bdd->query('SELECT nomV,dateDebut,refVisiteur,idVisiteur from Visiteur,Absence WHERE refVisiteur = idVisiteur AND dateDebut > DATE_SUB(CURDATE(), INTERVAL 1 DAY) and dateDebut < CURDATE()');
 	while($donnees = $reponse->fetch()){
 		echo "<p></p>";
@@ -44,7 +56,7 @@ $reponse = $bdd->query('SELECT * FROM Visiteur');
 		echo "<p></p>";
 
 		}
-	echo"Jamais Absent :";
+	echo"<h3>Jamais Absent :</h3>";
 	$answer = $bdd->query('SELECT nomV,idVisiteur from Visiteur  Where idVisiteur NOT IN (SELECT refVisiteur from Absence)');
 	while($data = $answer->fetch()){
 		echo "<p></p>";
@@ -52,4 +64,16 @@ $reponse = $bdd->query('SELECT * FROM Visiteur');
 		echo "<p></p>";
 		}
 		$reponse->closeCursor();
+		
+	
+		echo"<h3>Le plus Absent :</h3>";
+	$answer = $bdd->query('SELECT nomV,refVisiteur from Visiteur,CumulAbsences where idVisiteur = refVisiteur AND NJAV = ( SELECT MAX(NJAV) from CumulAbsences );
+');
+	while($data = $answer->fetch()){
+		echo "<p></p>";
+		echo $data['nomV'];
+		echo "<p></p>";
+		}
+		$reponse->closeCursor();
+
 
